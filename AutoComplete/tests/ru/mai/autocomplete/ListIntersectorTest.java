cdf9a8192b1ded7536759781a9a09007ed7d99c3
Safronov Alexey <safronov.aa@gmail.com>
2013-06-23 00:56:56 +0400

    Add fast lists intersector

---
+++ AutoComplete/tests/ru/mai/autocomplete/ListIntersectorTest.java
@@ -0,0 +1,107 @@
+package ru.mai.autocomplete;
+
+import junit.framework.Assert;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListIntersectorTest {
+    @Test
+    public void testGetIntersection() throws Exception {
+        {
+            List<Integer> first = new ArrayList<>();
+            first.add(0);
+            first.add(1);
+            first.add(2);
+
+            List<Integer> second = new ArrayList<>();
+            second.add(0);
+            second.add(1);
+            second.add(2);
+
+            List<Integer> result = new ArrayList<>();
+            result.add(0);
+            result.add(1);
+            result.add(2);
+
+            ListIntersector<Integer> intersector = new ListIntersector<>();
+            intersector.addList(first);
+            intersector.addList(second);
+
+            Assert.assertTrue(intersector.getIntersection(3).equals(result));
+        }
+
+        {
+            List<Integer> first = new ArrayList<>();
+            first.add(0);
+            first.add(1);
+            first.add(2);
+
+            List<Integer> second = new ArrayList<>();
+            second.add(0);
+            second.add(1);
+            second.add(2);
+
+            List<Integer> result = new ArrayList<>();
+            result.add(0);
+            result.add(1);
+
+            ListIntersector<Integer> intersector = new ListIntersector<>();
+            intersector.addList(first);
+            intersector.addList(second);
+
+            Assert.assertTrue(intersector.getIntersection(2).equals(result));
+        }
+
+        {
+            List<Integer> first = new ArrayList<>();
+            first.add(1);
+            first.add(2);
+
+            List<Integer> second = new ArrayList<>();
+            second.add(0);
+            second.add(1);
+
+            List<Integer> result = new ArrayList<>();
+            result.add(1);
+
+            ListIntersector<Integer> intersector = new ListIntersector<>();
+            intersector.addList(first);
+            intersector.addList(second);
+
+            Assert.assertTrue(intersector.getIntersection(2).equals(result));
+        }
+
+        {
+            List<Integer> first = new ArrayList<>();
+            first.add(0);
+            first.add(1);
+            first.add(2);
+            first.add(10);
+
+            List<Integer> second = new ArrayList<>();
+            second.add(1);
+            second.add(2);
+            second.add(3);
+            second.add(10);
+
+            List<Integer> third = new ArrayList<>();
+            third.add(2);
+            third.add(3);
+            third.add(4);
+            third.add(10);
+
+            List<Integer> result = new ArrayList<>();
+            result.add(2);
+            result.add(10);
+
+            ListIntersector<Integer> intersector = new ListIntersector<>();
+            intersector.addList(first);
+            intersector.addList(second);
+            intersector.addList(third);
+
+            Assert.assertTrue(intersector.getIntersection(10).equals(result));
+        }
+    }
+}
